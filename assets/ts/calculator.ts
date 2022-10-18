79e6b53e5140edf4edee6c97dccef249c96515a1
callsamu <samuelbree@gmail.com>
2022-10-18 16:57:18 -0300

    handle main calculator logic on calculator.ts implementing computeChance function

---
+++ assets/ts/calculator.ts
@@ -0,0 +1,10 @@
+import * as hash from "./hash";
+
+export function computeChance(name1: string, name2: string): number {
+  const limit = 10_000;
+
+  const m = hash.hashString(name1, limit);
+  const n = hash.hashString(name2, limit);
+  
+  return hash.hashNumber(m + n, limit) / 100;
+}
