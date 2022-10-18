e4683e16decf70104ceb5a8106103f39ab13916e
callsamu <samuelbree@gmail.com>
2022-10-18 15:00:33 -0300

    fix function type signatures

--- assets/ts/results.ts
+++ assets/ts/results.ts
@@ -1,6 +1,6 @@
 import type { FormResult } from "./form"
 
-export function hydrateResults(results: FormResult, container: HTMLElement) {
+export function hydrateResults(results: FormResult, container: Element) {
   const chance = container.querySelector(".chance")
   chance.innerHTML = results.chance.toFixed(2);
 }
