d3a2d6f1b171fb110958bd3a8ab0cb5df146840e
callsamu <samuelbree@gmail.com>
2022-11-12 17:11:25 -0300

    use texts on data templates based on calculator results

--- assets/ts/results.ts
+++ assets/ts/results.ts
@@ -1,3 +1,25 @@
+import * as params from "@params";
 import type { FormResult } from "./form"
 
-export function hydrateResults(results: FormResult, template: HTMLTemplateElement) {}
+interface ResultText {
+  headline: string
+  paragraph: string  
+}
+
+export function hydrateResults(results: FormResult, template: HTMLTemplateElement) {
+  try {
+    const texts: Array<ResultText> = params.texts.content;
+
+    const index = results.chance / (100 / texts.length)
+    console.log(index);
+    const text = texts[index];
+
+    const headline = template.content.querySelector('.result-text h2');
+    headline.innerHTML = text.headline;
+
+    const paragraph = template.content.querySelector('.result-text p');
+    paragraph.innerHTML = text.paragraph;
+   } catch(e) {
+    console.log(e);
+   }
+}
