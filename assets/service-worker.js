fe6c487f8ecda1580c666fe6398c61e66357060f
callsamu <samuelbree@gmail.com>
2022-11-19 22:45:47 -0300

    convert it to a progressive web app

---
+++ assets/service-worker.js
@@ -0,0 +1,10 @@
+if (typeof importScripts == "function") {
+    importScripts(
+        'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+    );
+
+    workbox.routing.registerRoute(
+        ({ request }) => request.destination === "image",
+        new workbox.strategies.NetworkFirst()
+    );
+};
