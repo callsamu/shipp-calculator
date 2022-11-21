77e17add1d51690fcf886bd7d9474aaf8715ae4c
callsamu <samuelbree@gmail.com>
2022-11-21 15:34:16 -0300

    add title and text to share data

--- assets/ts/app/share.ts
+++ assets/ts/app/share.ts
@@ -1,14 +1,31 @@
-export function onShareButtonClick(e: Event) {
-    if (!navigator.canShare) {
-        alert("Your browser does not support this feature.")
-        return;
-    }
+import * as params from "@params";
+import { FormResult } from "./form";
+import { renderTemplate } from "./template";
 
-    try {
-        navigator.share({
-            url: location.toString()
-        });
-    } catch (err) {
-        console.log(err);
+export function onShareButtonClick(results: FormResult) {
+    let shareContent = params?.texts?.share;
+    const shareText = (shareContent) ?
+        renderTemplate(shareContent.text, {
+            first: results.firstName,
+            second: results.secondName,
+            chance: results.chance,
+        }) : null;
+
+    return () => {
+
+        if (!navigator.canShare) {
+            alert("Your browser does not support this feature.")
+            return;
+        }
+
+        try {
+            navigator.share({
+                url: location.toString(),
+                title: shareContent?.title,
+                text: shareText,
+            });
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
